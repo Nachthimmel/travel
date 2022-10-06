bf82e1e852923ac2d7d31d818144de98eb2dfde2
Sky <art.kamalov.work@gmail.com>
2022-10-06 21:02:42 +0600

    video

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,13 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home_container">
-        <video src="/videos/video-1.mp4" muted loop autoPlay />
+        <video
+          src="https://github.com/Nachthimmel/travel/raw/gh-pages/videos/video-1.mp4"
+          muted
+          loop
+          autoPlay
+          type="video/mp4"
+        />
         <h1 className="home_h1">placeholder for h1</h1>
         <p className="home_p">placeholder for p tag</p>
         <div className="home_btns">
